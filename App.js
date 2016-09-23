1f608abd7fbc5006204e085bf7ec0627edafe1c7
Augustin <Augustinraj@users.noreply.github.com>
2016-09-24 00:06:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 function doArea() {
+//getting the values from the user what he enters in the textbox on clicking the button//
 var radius = document.findarea.rad.value;
+// Formula to calculate area of circle//
 var result = (3.14 * radius * radius);
+//Displaying Area of circle in console//
 console.log(result);
 }
